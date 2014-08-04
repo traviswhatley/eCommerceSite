5ea0e2c68a556b690ff6264a2558fced522de313
Travis Whatley <travis.whatley121@gmail.com>
2014-08-04 14:06:42 -0600

    updated

--- eCommerce/Controllers/CheckoutController.cs
+++ eCommerce/Controllers/CheckoutController.cs
@@ -13,49 +13,77 @@ namespace eCommerce.Controllers
 
         public ActionResult Index()
         {
-            return RedirectToAction("BillingAddress", "Checkout");
+            return RedirectToAction("BillingAddress");
         }
 
         [HttpGet]
         public ActionResult BillingAddress()
         {
-            return View(this.MyOrder.BillingAddress);
+            if (this.MyOrder.BillingAddress == null)
+            {
+                return View(this.MyOrder.BillingAddress);
+            }
+            return RedirectToAction("Review");
         }
 
         [HttpPost]
         public ActionResult BillingAddress(Models.Address address)
         {
-            db.Addresses.Add(address);
+            //db.Addresses.Add(address);
+            //db.SaveChanges();
+            this.MyOrder.BillingAddress = address;
             db.SaveChanges();
-            return View("ShippingAddress", "Checkout");
+            return RedirectToAction("ShippingAddress");
         }
 
         [HttpGet]
         public ActionResult ShippingAddress()
         {
-            return View(this.MyOrder.ShippingAddress);
+            if (this.MyOrder.ShippingAddress == null)
+            {
+                return View(this.MyOrder.ShippingAddress);    
+            }
+            return RedirectToAction("Review");
         }
 
         [HttpPost]
         public ActionResult ShippingAddress(Models.Address address)
         {
-            db.Addresses.Add(address);
+            //db.Addresses.Add(address);
+            //db.SaveChanges();
+            this.MyOrder.ShippingAddress = address;
             db.SaveChanges();
-            return View("Payment", "Checkout");
+            return RedirectToAction("Payment");
         }
 
         [HttpGet]
         public ActionResult Payment()
         {
-            return View(this.MyOrder.Payment);
+            if (this.MyOrder.Payment == null)
+            {
+                return View(this.MyOrder.Payment);    
+            }
+            else
+            {
+                return View("Payment");
+            }
+            return RedirectToAction("Review");
         }
 
         [HttpPost]
         public ActionResult Payment(Models.Payment payment)
         {
-            db.Payments.Add(payment);
+            //db.Payments.Add(payment);
+            //db.SaveChanges();
+            this.MyOrder.Payment = payment;
             db.SaveChanges();
-            return View("Index", "Home");
+            return RedirectToAction("Review");
+        }
+
+        [HttpGet]
+        public ActionResult Review()
+        {
+            return View(this.MyOrder);
         }
 
     }
